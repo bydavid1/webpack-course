55b07dcb342291291e288d6dad755cd99bdff234
Byron Jimenez <byronjimenez9911@gmail.com>
2021-01-08 19:52:08 -0600

    Babel config

---
+++ babel/src/js/text.js
@@ -0,0 +1,5 @@
+const text = () => {
+ document.body.innerHTML = "<h2>Text from javascript and HR and babel</h2>"
+}
+
+export default text
---
+++ babel/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const MiniCSSExtraxtPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Webpack = require('webpack')
+
+module.exports = {
+  entry: {
+    index : path.resolve(__dirname, 'src/js/index.js'),
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 8081
+  },
+  module : {
+    rules : [
+      {
+        test: /\.css$/,
+        use : [
+          // {
+          //   loader: MiniCSSExtraxtPlugin.loader
+          // },
+          'style-loader', //2
+          'css-loader', //1 ^
+        ]
+      },
+      {
+        test: /\.js$/,
+        use : [
+          'babel-loader' 
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins : [
+    new Webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'dev-server'
+    }),
+    // new MiniCSSExtraxtPlugin({
+    //   filename: 'css/[name].css'
+    // })
+  ]
+}
